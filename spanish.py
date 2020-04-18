c571404a0a77d2e64aea1e59d46b8c9502a29627
jonganebski <62702084+jonganebski@users.noreply.github.com>
2020-04-18 21:48:52 +0900

    Update spanish.py

--- spanish.py
+++ spanish.py
@@ -1,29 +1,44 @@
-# SPANISH VOCABULARY#
-
+import requests
+from bs4 import BeautifulSoup
 from random import *
+import html5lib
 
-print('Write in Spanish')
-right_answer = 'el ciudad'
+WORD_LIST = ['pequeno', 'nevera', 'mujer', 'sombrero', 'mensaje', 'baloncesto', 'preocupado', 'escuchar']
+URL = 'https://www.spanishdict.com/translate'
+headers = {'User-Agent': 'Mozilla/5.0'}
 
-for i in range(3):
-  question = input('the city: ').lower()
-  if question == right_answer:
-    print('correct')
-    break
-  elif 1 <= i < 3:
-    prop = round(len(right_answer) * 0.3)
-    random_nums = []
-    for s in range(prop):
-      random_nums.append(randint(0, len(right_answer) - 1))  # 문제점: 중복되는 수가 도출될 수 있음.
-    hint = 'hint: '
-    for n in range(len(right_answer)):
-      if n in random_nums:
-        hint += right_answer[n]
-      else:
-        if right_answer[n] == ' ':
-          hint += ' '
-        else:
-          hint += '?'
-    print(hint)
-  else:
-    print('wrong')
+def get_word():
+    radnom_num = randint(0, len(WORD_LIST)-1)
+    WORD = (WORD_LIST[radnom_num]).lower()
+    return WORD
+
+def req_bs4(WORD):
+    request_site = requests.get(f'{URL}/{WORD}', headers=headers)
+    soup_html = BeautifulSoup(request_site.content, 'html5lib')
+    soup_class = soup_html.find_all('div', {'class': '_2VJwFW1Z _2Gv8ITgB'})
+    return soup_class
+
+def get_meanings(WORD):
+    soup_class = req_bs4(WORD)
+    meanings = []
+    for i in soup_class:
+        soup_meaning = i.find('a').string
+        meanings.append(soup_meaning)
+    return meanings
+
+def pregunta():
+    WORD = get_word()
+    meanings_list = get_meanings(WORD)
+    text = f"'{meanings_list[0]}'"
+    if len(meanings_list) >= 2:
+        del meanings_list[0]
+        for i in meanings_list:
+            text += f" or '{i}'"
+    answer = input(f'What is {text} in Spanish? ')
+    if answer.lower() == WORD.lower():
+        print('Correct')
+    else:
+        print(f"Wrong. it is '{WORD}'.")
+
+pregunta()
+
