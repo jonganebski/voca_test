5fec0907248286226c044e52fd99cfe4a4e7a0c7
jonganebski <62702084+jonganebski@users.noreply.github.com>
2020-04-19 12:20:21 +0900

    Update add_words.py

--- add_words.py
+++ add_words.py
@@ -1,1 +1,21 @@
+def checking(new):
+    file = open('words_spanish.txt', 'r')
+    list = file.read().split(' ,')
+    file.close()
+    for i in list:
+        if i == new:
+            return False
+        else:
+            return True
 
+def adding_new_word(new):
+    file = open('words_spanish.txt', 'w')
+    check = checking(new)
+    if check:
+        file.write(f'{new}, ')
+        print(f"'{new}' is added to the list.")
+    else:
+        print("It already exists in the list.")
+    file.close()
+
+adding_new_word('trece')
